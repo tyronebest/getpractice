ec60b72dbb4c20907d64ea3e02fcb7ebd41cc36e
tyronebest <15best7961@pit.edu>
2016-03-24 18:11:22 +0000

    centered content

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ document.body.appendChild(content);
 //put some words in the content div
 
 content.innerHTML = "Hello World!";
+
+//increase font size
+content.style.fontSize = "50px";
+
+// change background 
+ content.style.backgroundColor = "teal";
+ 
+ // fit half the screen
+ content.style.width = "50%";
+ 
+ //center the text in the content div
+ content.style.textAlign ="center";
+ 
+ //
+ content.style.margin  = "10% auto";
+ 
+ //
+ content.style.fontFamily = "sans"
