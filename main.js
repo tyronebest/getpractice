f57ef7101c780c59c92cbbf5c6c3b99469b70653
tyronebest <15best7961@pit.edu>
2016-03-24 17:52:59 +0000

    made content

---
+++ main.js
@@ -0,0 +1,10 @@
+//lets build the html elements
+//create a div 
+var content = document.createElement("div");
+
+//place the content div in the body
+document.body.appendChild(content);
+
+//put some words in the content div
+
+content.innerHTML = "Hello World!";
